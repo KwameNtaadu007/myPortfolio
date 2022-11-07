fb4dc844ce777bae42d99eae4ef78b4de2dd23cb
KwameNtaadu007 <71965803+KwameNtaadu007@users.noreply.github.com>
2022-11-07 20:21:10 +0000

    images

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import logo from "./logo.svg";
+import { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/Menu";
+import { DISHES } from "./components/shared/dishes";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Confusion</NavbarBrand>
-        </div>
-      </Navbar>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Confusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
